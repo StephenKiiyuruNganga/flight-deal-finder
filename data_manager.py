c8a8c12396508f7e9cfda6d877e32ee1189121eb
StephenKiiyuruNganga <40911055+StephenKiiyuruNganga@users.noreply.github.com>
2022-09-11 15:57:16 +0300

    add google sheet communication

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,31 @@
+import requests
+
+SHEETY_API = "https://api.sheety.co/8e7a7dc2a8849028d5c9c103dd334896/flightDeals/prices"
+SHEETY_API_KEY = "St3vAn0v1c"
+SHEETY_HEADERS = {
+    "Authorization": f"Bearer {SHEETY_API_KEY}"
+}
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    # This class is responsible for talking to the Google Sheet.
+    def __init__(self):
+        self.data = None
+
+    def getFlightData(self):
+        response = requests.get(url=SHEETY_API, headers=SHEETY_HEADERS)
+        response.raise_for_status()
+        data = response.json()
+        self.data = data["prices"]
+
+    def updateRow(self, id, column_name, value):
+        body = {
+            "price": {
+                column_name: value
+            }
+        }
+        response = requests.put(
+            url=f"{SHEETY_API}/{id}", json=body, headers=SHEETY_HEADERS)
+        response.raise_for_status()
+        data = response.json()
+        print(data)
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from data_manager import DataManager
+from flight_search import FlightSearch
+from pprintpp import pprint
+
+
+dataManager = DataManager()
+dataManager.getFlightData()
+
+# pprint(dataManager.flight_data)
+
+sheet_data = dataManager.data
+
+for row in sheet_data:
+    code = FlightSearch.getIATACode()
+    if not row["iataCode"]:
+        dataManager.updateRow(row["id"], "iataCode", code)
